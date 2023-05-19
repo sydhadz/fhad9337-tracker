06ed2ce43733d4defa96b988d565ed115ca46bd6
sydfarrah <73594356+sydfarrah@users.noreply.github.com>
2023-05-19 12:29:53 +1000

    setup parcel and live server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express'); //import express package
 
 const app = express(); //initalise express framework
 
-app.use(express.static('public')); //Serve static files from the public folder
+app.use(express.static(__dirname + '/dist')); //Serve static files from the root directory folder
 
 app.get('/', function(req, res) { // Serve the index file for the root ("/") path
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
 
 let server = app.listen(8888, function(){ //Start the server, listening for incoming traffic and logging a message to the console
