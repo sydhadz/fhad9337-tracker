7646ebb67a8426156516cbc4282e00a2424df355
sydfarrah <73594356+sydfarrah@users.noreply.github.com>
2023-06-07 02:21:44 +1000

    added coding challenges code and some other sources for the form, pop-up, buttons and task list, and modified it. Also fixed SASS to work on live server

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fhad9337-tracker
+
+
+reference :
+https://www.w3schools.com/howto/howto_js_popup_form.asp --> how did i change this?
+https://www.teachucomp.com/special-characters-in-html-tutorial/ --> to type the "<" for the back/ exit form button
+https://www.youtube.com/watch?v=xJKiuovQcPA&ab_channel=NorbertBMWebDevelopment - to get sass working on npm run dev and opening with live server 
--- public/script.js
+++ public/script.js
@@ -1,11 +1,21 @@
 // Setting up variables for our HTML elements using DOM selection
-const form = document.getElementById("taskform");
+const formPopUp = document.getElementById("formPopUp");
+const form = document.getElementById("bookForm");
 const tasklist = document.getElementById("tasklist");
 
-form.addEventListener("submit", function(event) {
+function openForm() {
+  formPopUp.style.display = "block";
+};
+
+function closeForm() {
+  formPopUp.style.display = "none";
+};
+
+// saving the data in the form when submitting
+formPopUp.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  console.log(form.elements.taskType.value)
+  console.log(form.elements.taskType.value);
 
   addTask(
     form.elements.taskName.value,
@@ -13,23 +23,25 @@ form.addEventListener("submit", function(event) {
     form.elements.taskRate.value,
     form.elements.taskTime.value,
     form.elements.taskClient.value,
-  )
-  console.log(taskList)
-})
+  );
+  console.log(taskList);
+});
+
+//outside of the function and inside, because of an error where it thinks the item isnt defined and then doesn't append
+let item = document.createElement("li");
 
 function displayTask(task) {
   let item = document.createElement("li");
+
   item.setAttribute("data-id", task.id);
   item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
-
   tasklist.appendChild(item);
-
   // Clear the value of the input once the task has been added to the page
   form.reset();
 
   // Setup delete button DOM elements
   let delButton = document.createElement("button");
-  let delButtonText = document.createTextNode("Delete");
+  let delButtonText = document.createTextNode("🗑️");
   delButton.appendChild(delButtonText);
   item.appendChild(delButton); // Adds a delete button to every task
 
@@ -47,34 +59,36 @@ function displayTask(task) {
 
     item.remove(); // Remove the task item from the page when button clicked
     // Because we used 'let' to define the item, this will always delete the right element
+  })  
+};
 
-  })
+// SECTION 1 CODING CHALLENGE SCRIMBA CODE BELOW
+let checkboxElem = document.createElement("input");
+checkboxElem.setAttribute("type", "checkbox");
 
+item.insertBefore(checkboxElem, item.firstChild)
+
+checkboxElem.addEventListener("change", (event) => {
+  // Event listener callback function
+  let isChecked = event.target.checked;
   
-}
+  // change this for the favourite false/true 
+  taskList.forEach(function(taskArrayElement, taskArrayIndex){
+    if (taskArrayElement.id == item.getAttribute("data-id")){
+      if (isChecked) {
+        item.style.backgroundColor = 'rgb(220, 255, 220)';
+        taskArrayElement.billable = true;
+      } else {
+        item.style.backgroundColor = '#ffffff';
+        taskArrayElement.billable = false;
+      }
+    }
+  })
+  
+});
 
 
-
-
-// Create an object called 'task'
-// Populate the properties based on the provided data model
-
-// Commented out now the object creation is included in the function
-
-// var task = {
-//   name: "Initial Sketches",
-//   type: "Concept Ideation",
-//   id: Date.now(),
-//   date: new Date().toISOString(),
-//   rate: 50,
-//   time: 5,
-//   client: "Google"
-// }
-
-// console.log(task);
-
-
-// Create an array called 'taskList'
+// Create an array called 'taskList' - week 3 tutorial code
 var taskList = [];
 
 // Create a function called 'addTask'
@@ -84,18 +98,6 @@ var taskList = [];
 // Add the object to the taskList array
 
 function addTask(name, type, rate, time, client) {
-
-  // Creating the object with the usual property:value syntax
-  // Create task object 
-  // let task = {
-  //   name: name,
-  //   type: type,
-  //   id: Date.now(),
-  //   date: new Date().toISOString(),
-  //   rate: rate,
-  //   time: time,
-  //   client: client
-  // }
 
   // Creating the object, directly passing in the input parameters
   let task = {
@@ -113,8 +115,7 @@ function addTask(name, type, rate, time, client) {
 
 }
 
-// Call the function with test values for the input paramaters
-addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
+// Call the function with test values for the input paramaters - addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
 
 // Log the array to the console.
 console.log(taskList);
